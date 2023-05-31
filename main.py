08e4302edb919694f7c0b782b7b8a4f91be4d362
cusmy <55727972+cusmy@users.noreply.github.com>
2023-05-31 20:20:27 +0700

    tambah menu

--- main.py
+++ main.py
@@ -11,7 +11,20 @@ from rembg import remove
 from skimage.exposure import rescale_intensity
 from skimage.morphology import reconstruction
 from skimage.feature import graycomatrix, graycoprops
+import joblib
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, cross_validate
+from sklearn.cluster import KMeans
 
+
+data = pd.read_csv('out_all.csv',delimiter=',')
+X = data[['luas', 'tekstur']]
+y = data[['label']]
+X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False, test_size=0.2)
+k = 4
+kmeans = KMeans(n_clusters=k)
+kmeans.fit(X_train)
+cluster_labels = kmeans.predict(X_train)
 
 with open('neural network bersih kotor', 'rb') as file:
     mlp = pickle.load(file)
@@ -30,9 +43,8 @@ def run_data(image):
     vectorized = img_convert.reshape((-1,3))
     vectorized = np.float32(vectorized)
     criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    K = 2
     attempts=10
-    ret,label,center=cv.kmeans(vectorized,K,None,criteria,attempts,cv.KMEANS_PP_CENTERS)
+    ret,label,center=cv.kmeans(vectorized,2,None,criteria,attempts,cv.KMEANS_PP_CENTERS)
     center = np.uint8(center)
     res = center[label.flatten()]
     result_image2 = res.reshape((img_convert.shape))
@@ -47,14 +59,9 @@ def run_data(image):
     df = pd.DataFrame(propss)
     data = df.iloc[[0]]
     X_BK = features_scaler_bk.transform(data[['area_bbox', 'perimeter','area']])
-    data
     # X_BK = data[['area_bbox', 'perimeter','area','extent','eccentricity']]
     
     y_hat_bk = mlpbk.predict(X_BK)
-    if y_hat_bk == 'besar' :
-         st.write("Telor Besar")
-    else :
-         st.write("Telor kecil")
     # K = 15
     # attempts=10
     # ret,label,center=cv.kmeans(vectorized,K,None,criteria,attempts,cv.KMEANS_PP_CENTERS)
@@ -72,32 +79,55 @@ def run_data(image):
     ASM = np.array(graycoprops(graycom, 'ASM'))
     d = {'contrast': contrast[0], 'dissimilarity': dissimilarity[0], 'homogeneity' : homogeneity[0],'energy':energy[0],'correlation' : correlation[0],'ASM' : ASM[0]}
     df = pd.DataFrame(data=d)
-    df
     # X = df[['contrast', 'dissimilarity','homogeneity','energy','correlation','ASM']]
     X = features_scaler_mlp.transform(df[['contrast', 'dissimilarity','homogeneity']])
     yhat = mlp.predict(X)
-    if yhat == 'bersih' :
-         st.write("Telor Bersih")
+    # if y_hat_bk == 'besar' and yhat == 'bersih':
+    #     predik = "1"
+    # elif y_hat_bk == 'kecil' and yhat == 'bersih':
+    #     predik = "2"
+    # elif  y_hat_bk == 'besar' and yhat == 'kotor':
+    #     predik = "3"
+    # elif y_hat_bk == 'kecil' and yhat == 'kotor':
+    #     predik = "4"
+    values = mlpbk.predict_proba(X_BK[:1])
+    values = np.max(values)
+    vvalue = mlp.predict_proba(X[:1])
+    vvalue = np.max(vvalue)
+    if y_hat_bk == 'besar':
+        luas = 1 * values
+    else : 
+        luas = 2 * values
+    if yhat == 'bersih':
+        tekstur = 1 * vvalue
     else :
-         st.write("Telor Kotor")
-      
-    col11, col22, col33 = st.columns(3)
-    with col11:
-        st.image(
-        image,
-        caption=f"Citra Asli",
-        use_column_width=True,
-    )
+        tekstur = 2 * vvalue
+    # hasil = {'luas': luas, 'tekstur': tekstur}
+    # ddata = pd.DataFrame.from_records([{ 'luas': luas, 'tekstur': tekstur}])
 
-    with col22:
-        st.image(
-        result_image2,
-        caption=f"Kmeans K=2",
-        use_column_width=True,
-    )
+    x0_values = np.linspace(0.4, 2.1, 100)
+    x1_values = np.linspace(0.4, 2.1, 100)
+    coords = np.asarray(np.meshgrid(x0_values, x1_values)).T.reshape(-1,2)
+    for i, label in enumerate(cluster_labels):
+        plt.text(X_train.iloc[i, 0], X_train.iloc[i, 1], str(label), color='red', fontsize=8, ha='center')
+        
+    plt.scatter(X_train.iloc[:, 0], X_train.iloc[:, 1], c=cluster_labels, cmap='bwr')
+    plt.scatter(luas, tekstur, c='g', marker='x', s=300)
+    plt.scatter(coords[:,0], coords[:,1], c=kmeans.predict(coords), cmap='tab10', alpha=0.04)
+    plt.xlabel('Luas')
+    plt.ylabel('Tekstur')
+    plt.title('K-Means Clustering')
 
-    with col33:
-        st.write()
+    st.pyplot(plt)
+    if y_hat_bk == 'besar' and yhat == 'bersih':
+        predik = "Lottemart"
+    elif y_hat_bk == 'kecil' and yhat == 'bersih':
+        predik = "Indomart"
+    elif  y_hat_bk == 'besar' and yhat == 'kotor':
+        predik = "Pasar"
+    elif y_hat_bk == 'kecil' and yhat == 'kotor':
+        predik = "Buruk"
+    st.write('dari hasil diatas telur tersebut termasuk cluster ***{}*** ditandai dengan mark ***X*** berwarna hijau'.format(predik))
     
 
 
